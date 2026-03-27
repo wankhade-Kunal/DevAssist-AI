a0fd15edffb4ebd17452b73616adb66af8c9ba5f
Kunal wankhade <145186647+wankhade-Kunal@users.noreply.github.com>
2026-03-27 15:48:09 +0530

    Delete client directory

--- client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-white dark:bg-slate-900 text-black dark:text-gray-400 py-6 text-center border-t border-gray-300 dark:border-gray-700">
-      <p>© 2026 DevAssist AI. All rights reserved.</p>
-    </footer>
-  )
-}
-
-export default Footer
