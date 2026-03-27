d5974aacfd48f03f3ec96cbfb1a0157456461b32
Kunal wankhade <145186647+wankhade-Kunal@users.noreply.github.com>
2026-03-27 16:03:00 +0530

    Delete server directory

--- server/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendMessage } from "../controllers/chatController.js";
-
-const router = express.Router();
-
-router.post("/", sendMessage);
-
-export default router;
